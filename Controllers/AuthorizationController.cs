6b58f3987c7cefdc2ef25d183a6af84808f1c9ef
Eric <ericj037466@gmail.com>
2022-08-14 21:58:21 +0800

    add jwt files

---
+++ Controllers/AuthorizationController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTNet6.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthorizationController : ControllerBase
+    {
+        private readonly ILogger<AuthorizationController> logger;
+        private readonly IConfiguration configuration;
+        public AuthorizationController(ILogger<AuthorizationController> logger
+            , IConfiguration configuration)
+        {
+            this.logger = logger;
+            this.configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UserInfo request)
+        {
+
+            var claims = new[] {
+                        new Claim(JwtRegisteredClaimNames.Sub, configuration["Jwt:Subject"]),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                        new Claim("UserId", request.userId),
+                        new Claim("DisplayName", request.name),
+                        new Claim("UserName", request.name)
+                    };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                configuration["Jwt:Issuer"],
+                configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.UtcNow.AddMinutes(10),
+                signingCredentials: signIn);
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+
+            //no match 
+            //return BadRequest("Invalid credentials");
+
+            //other
+            //return BadRequest();
+        }
+
+        public class UserInfo
+        {
+            public string userId { get; set; }
+            public string name { get; set; }
+            public string password { get; set; }
+        }
+
+        public class AuthResponse
+        {
+            public string authorization { get; set; }
+            public int expired { get; set; }
+        }
+    }
+
+
+
+    //var claims = new[] {
+    //                    new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+    //                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+    //                    new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+    //                    new Claim("UserId", user.UserId.ToString()),
+    //                    new Claim("DisplayName", user.DisplayName),
+    //                    new Claim("UserName", user.UserName),
+    //                    new Claim("Email", user.Email)
+    //                };
+}
---
+++ Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTNet6.Controllers
+{
+    
+    [Route("api/test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        { 
+        }
+        [HttpGet, Authorize]
+        public async Task<IActionResult> Get([FromQuery] string request)
+        {
+            
+            return Ok();
+
+        }
+    }
+}
